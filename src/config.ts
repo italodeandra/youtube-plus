184f4a5a7fa881990e57b9f236c9fcbd01131e26
Ítalo Andrade <italodeandra@gmail.com>
2021-04-03 18:28:34 +0200

    receive base url env var on the cra build plan

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
 const config = {
   youtubeRegex: /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/,
   databaseUrl: process.env.DATABASE_URL,
-  baseUrl: process.env.BASE_URL || "http://localhost:3000",
+  baseUrl:
+    process.env.BASE_URL ||
+    process.env.REACT_APP_BASE_URL ||
+    "http://localhost:3000",
 }
 
 export default config
