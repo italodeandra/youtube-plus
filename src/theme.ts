430bb2b742f04be5a9ce63551892922b0552cc3b
Ítalo Andrade <italodeandra@gmail.com>
2021-07-05 10:26:27 +0200

    fix colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,13 @@
 import "@fontsource/inter/variable-full.css"
 import createTheme from "@italodeandra/pijama/styles/createTheme"
+import { darken, lighten } from "@material-ui/core/styles"
 
 const theme = createTheme({
   palette: {
     primary: {
       main: "#c00",
+      dark: darken("#c00", 0.1),
+      light: lighten("#c00", 0.1),
     },
   },
   components: {
