aa46e7ccad9ddf7e9ea731f41bc06620f3aad762
Ítalo Andrade <italodeandra@gmail.com>
2021-07-05 10:10:54 +0200

    only try to import `mongodb-memory-server` if no databaseUrl

--- src/middlewares/connectDb.ts
+++ src/middlewares/connectDb.ts
@@ -1,23 +1,30 @@
 import mongoose from "mongoose"
 import config from "../config"
-import mongoMemoryServer from "../utils/mongoMemoryServer"
 import { NextApiRequest, NextApiResponse } from "next"
 
-const connectDb = async (req: NextApiRequest, res: NextApiResponse, next: any) => {
+const connectDb = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: any
+) => {
   if (mongoose.connections[0].readyState) {
     // Use current db connection
     return next()
   }
+
+  if (!config.databaseUrl) {
+    const { MongoMemoryServer } = await import("mongodb-memory-server")
+    const mongoMemoryServer = new MongoMemoryServer()
+    config.databaseUrl = await mongoMemoryServer.getUri()
+  }
+
   // Create new db connection
-  await mongoose.connect(
-    config.databaseUrl || (await mongoMemoryServer.getUri()),
-    {
-      useUnifiedTopology: true,
-      // useFindAndModify: false,
-      // useCreateIndex: true,
-      useNewUrlParser: true,
-    }
-  )
+  await mongoose.connect(config.databaseUrl, {
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
   return next()
 }
 
--- src/utils/mongoMemoryServer.ts
+++
@@ -1,5 +0,0 @@
-import { MongoMemoryServer } from "mongodb-memory-server"
-
-const mongoMemoryServer = new MongoMemoryServer()
-
-export default mongoMemoryServer
